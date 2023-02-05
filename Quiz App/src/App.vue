55741c0fcc58ef6a19212e5d2b1e30fff63f7f08
Catalin Dehelean <drcatalin00@gmail.com>
2023-02-05 10:51:32 +0200

    init

---
+++ Quiz App/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+	<div class="wrapper">
+		<nav>
+			<RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/about">About</RouterLink>
+		</nav>
+
+		<RouterView />
+	</div>
+</template>
+
+<style scoped>
+.wrapper {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+}
+nav {
+	padding: 20px;
+}
+
+nav a.router-link-exact-active {
+	color: black;
+}
+nav a.router-link-exact-active:hover {
+	background-color: transparent;
+}
+
+nav a {
+	display: inline-block;
+	padding: 0 1rem;
+	color: black;
+}
+nav a:hover {
+	color: rgb(77, 76, 76);
+}
+</style>
---
+++ Quiz App/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+	</div>
+</template>
+
+<style scoped>
+.about {
+	min-height: 100%;
+	display: flex;
+	align-items: center;
+	color: black;
+}
+</style>
